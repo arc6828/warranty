b5864c9504c7c070807fe0bd467a026fd45feee5
deer2018 <deer11485@gmail.com>
2019-11-04 23:22:15 +0700

    deer2018

--- resources/views/schedule/edit.blade.php
+++ resources/views/schedule/edit.blade.php
@@ -1,10 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
     <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
-
+        <div class="row justify-content-center">
+            
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">Edit Schedule #{{ $schedule->id }}</div>
--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
     <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
+        <div class="row justify-content-center">
+            
 
             <div class="col-md-9">
                 <div class="card">
