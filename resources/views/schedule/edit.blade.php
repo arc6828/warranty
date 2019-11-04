03161a0a34476300347ac730a7647b253da0ed37
boomsuphan <52275366+boomsuphan@users.noreply.github.com>
2019-11-05 01:38:36 +0700

    Revert "Boomsuphan"
    This reverts commit 8900d46b97dac191f7db325069e36675ccb53cdf.

--- resources/views/schedule/edit.blade.php
+++ resources/views/schedule/edit.blade.php
@@ -1,10 +1,9 @@
-@extends('layouts.app')
+@extends('layout.main')
 
 @section('content')
     <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
-
+        <div class="row justify-content-center">
+            
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">Edit Schedule #{{ $schedule->id }}</div>
