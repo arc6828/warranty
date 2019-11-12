2b9689732b876ed58fb6642b9de448d1cf01b12c
arc6828 <chavalit.kow@gmail.com>
2019-11-12 17:06:27 +0700

    Theme Fix

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,14 +1,12 @@
 <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset ('img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset ('img/favicon.ico') }}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Light Bootstrap Dashboard - Free Bootstrap 4 Admin Dashboard by Creative Tim</title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-    <!-- CSS Files -->
-    <link href="{{ asset ('css/bootstrap.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset ('css/light-bootstrap-dashboard.css?v=2.0.0 ') }}" rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{ asset ('css/demo.css') }}" rel="stylesheet" />
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset ('img/apple-icon.png') }}">
+<link rel="icon" type="image/png" href="{{ asset ('img/favicon.ico') }}">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title>Light Bootstrap Dashboard - Free Bootstrap 4 Admin Dashboard by Creative Tim</title>
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+<!-- CSS Files -->
+<link href="{{ asset ('css/bootstrap.min.css') }}" rel="stylesheet" />
+<link href="{{ asset ('css/light-bootstrap-dashboard.css?v=2.0.0 ') }}" rel="stylesheet" />
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,18 +4,19 @@
 <head>
   @include('layout.head')
 </head>
-<body id="page-top">
-  <div id="wrapper">
-  @include('layout.sidebar')
-  <div class="main-panel">
-    @include('layout.navbar')
-      <div id="content">
-      
-        <div class="container-fluid pt-4">
-          @yield('content')
+<body >
+  <div class="wrapper">
+    @include('layout.sidebar')
+    <div class="main-panel">
+        @include('layout.navbar')
+        <div class="content" >
+        
+          <div class="container-fluid pt-4">
+            @yield('content')
+          </div>
         </div>
+        @include('layout.footer')     
       </div>
-      @include('layout.footer')     
     </div>
   </div>
   @include('layout.js')
