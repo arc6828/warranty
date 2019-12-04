0d341cb86fb14205c8a32322690f54365e98762d
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2019-12-04 18:25:04 +0700

    1,2  *** คือยังไม่ได้ทำ/ไม่เสร็จ

--- resources/views/energy/form1.blade.php
+++ resources/views/energy/form1.blade.php
@@ -8,27 +8,150 @@
 
             <div class="col-md-12">
             
-                <div class="card mb-4">
-                    <div class="card-header">Card 1</div>
+            <div class="card mb-4">
+                    <div class="card-header">ประจำปี 25xx</div>
                     <div class="card-body">
-                        Hello world
-
+                    <form class="form-inline">
+  <div class="form-group mb-2">
+    <input type="text" readonly class="form-control-plaintext" value="ชื่อนิติบุคคล">
+    <input type="text" readonly class="form-control-plaintext" value="บริษัท">
+    <input type="text" class="form-control"  placeholder="">
+    <input type="text" readonly class="form-control-plaintext" value="จำกัด">
+  </div>
+  <div class="form-group mb-2">
+    <input type="text" readonly class="form-control-plaintext" value="ชื่อโรงงาน">
+    <input type="text" readonly class="form-control-plaintext" value="โรงงาน">
+    <input type="text" class="form-control"  placeholder="">
+  </div>
+  <div class="form-group mb-2">
+    <input type="text" readonly class="form-control-plaintext" value="TSIC-ID">
+    <input type="text" readonly class="form-control-plaintext" value="">
+    <input type="text" class="form-control"  placeholder="">
+  </div>
+  
+</form>
                     </div>
                 </div>
+                <div class="card mb-4">
+                    <div class="card-header"></div>
+                    <div class="card-body">
+
+                    <div class="form-group " >ประธานคณะทำงานด้านการจัดการพลังงาน</div>
+                    <form class="form-inline">
+                        <div class="form-group mb-2">
+                     <input type="text" readonly class="form-control-plaintext" value="ลงชื่อ">
+                    <input type="text" class="form-control"  placeholder="">
+                        </div>
+                        </form>
+                        <form class="form-inline">
+                        <div class="form-group mb-2">
+                    <input type="text" readonly class="form-control-plaintext" value="วันที่">
+                    <input type="date" class="form-control"  placeholder="">
+                        </div> 
+                        </form>
+                    </div>
+
+
+                    <div class="card-body">
+
+
+                    <div class="form-group " > ผู้รับผิดชอบด้านพลังงาน</div>
+                    <form class="form-inline">
+                        <div class="form-group mb-2">
+                     <input type="text" readonly class="form-control-plaintext" value="ลงชื่อ">
+                    <input type="text" class="form-control"  placeholder="">
+                    </div></form>
+                    <form>
+                    <div class="form-group">
+                    <div class="form-group " >ตำแหน่ง ผู้รับผิดชอบดานพลังงานสามัญ ทะเบียนเลขที่</div>
+                        <input type="text" class="form-control"  placeholder="">
+                    </div>
+                    </form>
+                    <form class="form-inline">
+                        <div class="form-group mb-2">
+                    <input type="text" readonly class="form-control-plaintext" value="วันที่">
+                    <input type="date" class="form-control"  placeholder="">
+                        </div> 
+                        </form>
+
+                        <form class="form-inline">
+                        <div class="form-group mb-2">
+                     <input type="text" readonly class="form-control-plaintext" value="ลงชื่อ">
+                    <input type="text" class="form-control"  placeholder="">
+                    </div></form>
+                    <form>
+                    <div class="form-group">
+                    <div class="form-group " >ตำแหน่ง ผู้รับผิดชอบดานพลังงานอาวุโส ทะเบียนเลขที่</div>
+                        <input type="text" class="form-control"  placeholder="">
+                    </div>
+                    </form>
+                    <form class="form-inline">
+                        <div class="form-group mb-2">
+                    <input type="text" readonly class="form-control-plaintext" value="วันที่">
+                    <input type="date" class="form-control"  placeholder="">
+                        </div> 
+                        </form>
+                        
+                        
+                    </div>
+                    <div class="card-body">
+
+                    <div class="form-group " > เจ้าของโรงงาน</div>
+                    <form class="form-inline">
+                   <div class="form-group mb-2">
+                   <input type="text" readonly class="form-control-plaintext" value="ลงชื่อ">
+                   <input type="text" class="form-control"  placeholder="">
+                   </div>
+                   </form>
+                   <form class="form-inline">
+                   <div class="form-group mb-2">
+                   <input type="text" readonly class="form-control-plaintext" value="วันที่">
+                   <input type="date" class="form-control"  placeholder="">
+                   </div> 
+                   </form>
+                   </div>
+
+                </div>
+            
 
                 <div class="card mb-4">
-                    <div class="card-header">Card 2</div>
+                    <div class="card-header"></div>
                     <div class="card-body">
-                        Hello world
+                    <div class="form-group " >******</div>
                         
 
                     </div>
                 </div>
 
                 <div class="card mb-4">
-                    <div class="card-header">Card n</div>
+                    <div class="card-header"></div>
                     <div class="card-body">
-                        Hello world
+                    <div class="form-group mb-2 " >โครงสร้างคณะทำงานดานการจัดการพลังงาน</div>
+                    <form>
+                    <div class="form-group mb-2">
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+                    </form>
+                    <div><label for="exampleFormControlFile1" >( ใส่ผังโครงสร้างคณะทำงานดานการจัดการพลังงาน )</label></div></div>
+
+                
+                    <div class="card-body">
+                    <div class="form-group mb-2 " >การแต่งตั้งคณะทำงานด้านการจัดพลังงาน และอำนาจหน้าที่ความรับผิดชอบ</div>
+                    <form>
+                    <div class="form-group mb-2">
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+
+                    </form>
+                    <div><label for="exampleFormControlFile1" >( ใส่คำสั่งแต่งตั้งคณะทำงานด้านการจัดพลังงาน )</label></div>
+                        
+                    ********
+                    </div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-header"></div>
+                    <div class="card-body">
+                    ********
                         
 
                     </div>
