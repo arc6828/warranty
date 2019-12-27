fbcd8e00268fed1e2a752ce5a5f717d68b44d8ee
Chavalit Koweerawong <chavalit.kow@gmail.com>
2019-12-27 14:06:10 +0700

    Update 2019_12_27_060015_create_energy_factory_informations_table.php

--- database/migrations/2019_12_27_060015_create_energy_factory_informations_table.php
+++ database/migrations/2019_12_27_060015_create_energy_factory_informations_table.php
@@ -15,7 +15,7 @@ class CreateEnergyFactoryInformationsTable extends Migration
         Schema::create('energy_factory_informations', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('factory_size');
+            $table->string('factory_size')->nullable();
             $table->text('factory_address')->nullable();
             $table->string('factory_tel')->nullable();
             $table->string('factory_fax')->nullable();
@@ -24,7 +24,7 @@ class CreateEnergyFactoryInformationsTable extends Migration
             $table->string('office_tel')->nullable();
             $table->string('office_fax')->nullable();
             $table->string('office_email')->nullable();
-            $table->string('factory_type');
+            $table->string('factory_type')->nullable();
             $table->text('factory_employee')->nullable();
             $table->text('factory_operation_time')->nullable();
             $table->integer('user_id')->nullable();
